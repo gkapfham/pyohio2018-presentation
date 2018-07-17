5e0e8c3b3647ea9215fabfcce1e61c9c7dd211bf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-17 15:02:31 -0400

    Adding in all of the JavaScript files and many of the dotfiles.

---
+++ src/components/notes.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// TODO(540): Refactor to non-deprecated lifecycle methods.
+// https://github.com/FormidableLabs/spectacle/issues/540
+// - componentWillMount
+// eslint-disable-next-line react/no-deprecated
+export default class Notes extends Component {
+  static contextTypes = {
+    store: PropTypes.object,
+    slideHash: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    updateNotes: PropTypes.func
+  };
+
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  };
+
+  componentWillMount() {
+    const { store, slideHash: parentSlide, updateNotes } = this.context;
+    const currentSlide = store.getState().route.slide;
+
+    // updateNotes is only defined when this component is wrapped in
+    // a Presenter.
+    // Also, the type of parentSlide is either string or number based
+    // on the parent slide having an id or not.
+    if (updateNotes && currentSlide === `${parentSlide}`) {
+      updateNotes(this.props.children);
+    }
+  }
+
+  render() {
+    return false;
+  }
+}
